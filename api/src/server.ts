29c6049e97d66787d782bb218368799a5d6bee7e
wellington <wellingtonslpbi@hotmail.com>
2020-10-26 20:14:41 -0300

    chore: handle global exceptions

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,28 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.log(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server is running on port: 3333');
