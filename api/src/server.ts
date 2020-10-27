0260b7fd822475d6a007d8e61c8cf46a33f182df
wellington <wellingtonslpbi@hotmail.com>
2020-10-26 21:14:22 -0300

    feat: verify if user exists and hashing password

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import AppError from './errors/AppError';
 import './database';
--- api/src/services/CreateUserService.ts
+++ api/src/services/CreateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
+import AppError from '../errors/AppError';
 
 interface Request {
   name: string;
@@ -11,10 +13,18 @@ class CreateUserService {
   public async execute({ name, email, password }: Request): Promise<User> {
     const usersRepository = getRepository(User);
 
+    const userExists = await usersRepository.findOne({ where: { email } });
+
+    if (userExists) {
+      throw new AppError('User already exists');
+    }
+
+    const passwordHashed = await hash(password, 8);
+
     const user = await usersRepository.create({
       name,
       email,
-      password,
+      password: passwordHashed,
     });
 
     await usersRepository.save(user);
